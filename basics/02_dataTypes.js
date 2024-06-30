04810e7afbe42d3c33c61871cafd82ba3eecc932
CypherSatyam <satya.dubey.it@gmail.com>
2024-06-30 07:20:17 +0000

    Datatypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";  //treat all JS code as newer version
+
+/*alert(3 +3); it will not work because we are not on browser , we are on node*/
+
+/*data types
+
+number
+bigInt
+String
+boolean
+null    type object 
+undefinfed type undefined
+symbol
+
+object*/
+
+console.log(typeof "Satya,");
---
+++ basics/O1_Variables.js
@@ -0,0 +1,8 @@
+const Country = "India";
+var State = 12342;
+let Email = "abc@123.com";
+colony  = "Matigarh";
+/* Prefer not to use var beacuse of issue in block scope and functiona; scope*/
+
+state = "Satyam";
+console.log(state);
